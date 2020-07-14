4a5b8f01f26870278362f29ccdc6c73586e42aa2
mosne <gmosne@gmail.com>
2020-07-14 17:46:10 +0200

    :sunglasses: run first command install every thing and exclude node_modules from dropbox backup

---
+++ runfirst.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+LOCALDIR="$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
+NODEMODULE="node_modules"
+
+if [ -d "${NODEMODULE}" ]
+then
+    echo "Directory ./node_modules exists."
+else
+    mkdir "${NODEMODULE}"
+    echo "Directory node_modules created"
+fi
+
+echo "Directory node_modules abspath: ${LOCALDIR}${NODEMODULE}"
+xattr -w com.dropbox.ignored 1 "${LOCALDIR}${NODEMODULE}"
+echo "Directory excluded from dropbox."
+
+pnpm install
+pnpm run build
+
+

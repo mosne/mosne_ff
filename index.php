92c9f175e79159a5b8f4deb02750d2ccc101ea55
mosne <gmosne@gmail.com>
2020-08-02 18:04:30 +0200

    fix footer and index default markup

--- index.php
+++ index.php
@@ -2,24 +2,24 @@
 <main id="bd">
 	<article class="page">
 
-			<?php query_posts( 'posts_per_page=-1' ); if ( have_posts() ) : ?>
-				<?php
-				while ( have_posts() ) :
-					the_post();
-					?>
-				 <div class="grid">
-					<div class="item"><div class="pallino" style="background-color:<?php the_field( 'color' ); ?>"></div></div>
-					<div class="item"><?php m_icon( 'facebook' ); ?></div>
-					<div class="item">
-						<a href="<?php the_permalink();?>"><?php the_title(); ?></a></div>
-					<?php $link = get_field( 'link' ); ?>
-					<div class="item"><a href="<?php echo $link; ?>" target="_black"><?php echo nicelink_url( $link ); ?></a></div>
-					<div class="item"><?php the_field( 'size' ); ?> | <?php m_edit(); ?></div>
-				</div>
-					<?php
-			endwhile;
-endif;
+	<?php
+		query_posts( 'posts_per_page=-1' );
+	if ( have_posts() ) :
+		while ( have_posts() ) :
+			the_post();
 			?>
+
+				<div class="item"><div class="pallino" style="background-color:<?php the_field( 'color' ); ?>"></div></div>
+				<div class="item"><?php m_icon( 'facebook' ); ?></div>
+				<div class="item">
+					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
+				<?php $link = get_field( 'link' ); ?>
+				<div class="item"><a href="<?php echo $link; ?>" target="_black"><?php echo nicelink_url( $link ); ?></a></div>
+				<div class="item"><?php the_field( 'size' ); ?> | <?php m_edit(); ?></div>
+				<?php
+		endwhile;
+		endif;
+	?>
 
    </article>
 </main>

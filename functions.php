07c90e2734708f45ed2046ef7b9453e895b19f5e
ptesei <gmosne@gmail.com>
2020-08-29 15:15:04 +0200

    fix path

--- functions.php
+++ functions.php
@@ -9,18 +9,18 @@ show_admin_bar( false );
 $role_object = get_role( 'editor' );
 $role_object->add_cap( 'edit_theme_options' );
 
-require_once __DIR__ . 'inc/services.php';
-require_once __DIR__ . 'inc/register-menu.php';
-require_once __DIR__ . 'inc/register-sidebar.php';
-require_once __DIR__ . 'inc/utilities.php';
-require_once __DIR__ . 'inc/polylang.php';
-require_once __DIR__ . 'inc/acf.php';
-require_once __DIR__ . 'inc/woocommerce.php';
+require_once __DIR__ . '/inc/services.php';
+require_once __DIR__ . '/inc/register-menu.php';
+require_once __DIR__ . '/inc/register-sidebar.php';
+require_once __DIR__ . '/inc/utilities.php';
+require_once __DIR__ . '/inc/polylang.php';
+require_once __DIR__ . '/inc/acf.php';
+require_once __DIR__ . '/inc/woocommerce.php';
 
 // m functions
-require_once __DIR__ . 'inc/m_image.php';
-require_once __DIR__ . 'inc/m_section.php';
-require_once __DIR__ . 'inc/m_icon.php';
+require_once __DIR__ . '/inc/m_image.php';
+require_once __DIR__ . '/inc/m_section.php';
+require_once __DIR__ . '/inc/m_icon.php';
 
 /*
 function my_theme_deregister_plugin_assets_header() {

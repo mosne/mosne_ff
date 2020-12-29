69782e120e807c63c2f5387b05e5d881d4b02d8a
ptesei <gmosne@gmail.com>
2020-12-29 19:59:50 +0100

    feat (image) twig template

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
  * @since   Timber 0.1
  */
 
-show_admin_bar( false );
+// show_admin_bar( false );
 
 //get the the role object
 $role_object = get_role( 'editor' );
@@ -35,7 +35,7 @@ require_once __DIR__ . '/inc/m_icon.php';
 //$composer_autoload = __DIR__ . '/vendor/autoload.php';
 //if ( file_exists( $composer_autoload ) ) {
 //	require_once $composer_autoload;
-	$timber = new Timber\Timber();
+//	$timber = new Timber\Timber();
 //}
 
 /**
@@ -63,7 +63,7 @@ if ( ! class_exists( 'Timber' ) ) {
 /**
  * Sets the directories (inside your theme) to find .twig files
  */
-Timber::$dirname = array( 'templates', 'views' );
+Timber::$dirname = array( 'views' );
 
 /**
  * By default, Timber does NOT autoescape values. Want to enable Twig's autoescape?
@@ -79,20 +79,9 @@ Timber::$autoescape = false;
 class StarterSite extends Timber\Site {
 	/** Add timber support. */
 	public function __construct() {
-		add_action( 'after_setup_theme', array( $this, 'theme_supports' ) );
 		add_filter( 'timber/context', array( $this, 'add_to_context' ) );
 		add_filter( 'timber/twig', array( $this, 'add_to_twig' ) );
-		add_action( 'init', array( $this, 'register_post_types' ) );
-		add_action( 'init', array( $this, 'register_taxonomies' ) );
 		parent::__construct();
-	}
-	/** This is where you can register custom post types. */
-	public function register_post_types() {
-
-	}
-	/** This is where you can register custom taxonomies. */
-	public function register_taxonomies() {
-
 	}
 
 	/** This is where you add some context
@@ -100,66 +89,9 @@ class StarterSite extends Timber\Site {
 	 * @param string $context context['this'] Being the Twig's {{ this }}.
 	 */
 	public function add_to_context( $context ) {
-		$context['foo']   = 'bar';
-		$context['stuff'] = 'I am a value set in your functions.php file';
-		$context['notes'] = 'These values are available everytime you call Timber::context();';
-		$context['menu']  = new Timber\Menu();
-		$context['site']  = $this;
+		$context['menu'] = new Timber\Menu();
+		$context['site'] = $this;
 		return $context;
-	}
-
-	public function theme_supports() {
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
-
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support( 'title-tag' );
-
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
-		add_theme_support( 'post-thumbnails' );
-
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
-		add_theme_support(
-			'html5',
-			array(
-				'comment-form',
-				'comment-list',
-				'gallery',
-				'caption',
-			)
-		);
-
-		/*
-		 * Enable support for Post Formats.
-		 *
-		 * See: https://codex.wordpress.org/Post_Formats
-		 */
-		add_theme_support(
-			'post-formats',
-			array(
-				'aside',
-				'image',
-				'video',
-				'quote',
-				'link',
-				'gallery',
-				'audio',
-			)
-		);
-
-		add_theme_support( 'menus' );
 	}
 
 	/** This Would return 'foo bar!'.

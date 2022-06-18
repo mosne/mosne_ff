58d108e17708a6875781ff02dc145ec6c3f9db25
Paolo <ptesei@beapi.fr>
2022-06-18 15:12:13 +0200

    add button ad colums default style

--- inc/editor-block-styles.php
+++ inc/editor-block-styles.php
@@ -6,8 +6,29 @@
 register_block_style(
 	'core/button',
 	[
+		'name'  => 'button-arrow',
+		'label' => __( 'Arrow', 'business-france' ),
+	]
+);
+register_block_style(
+	'core/button',
+	[
 		'name'  => 'reverse',
 		'label' => __( 'Reverse', 'mosne' ),
+	]
+);
+register_block_style(
+	'core/button',
+	[
+		'name'  => 'reverse-outline',
+		'label' => __( 'Reverse outline', 'mosne' ),
+	]
+);
+register_block_style(
+	'core/button',
+	[
+		'name'  => 'text-arrow',
+		'label' => __( 'Text', 'mosne' ),
 	]
 );
 register_block_style(
@@ -15,5 +36,27 @@ register_block_style(
 	[
 		'name'  => 'big',
 		'label' => __( 'Big', 'mosne' ),
+	]
+);
+//columns
+register_block_style(
+	'core/columns',
+	[
+		'name'  => 'reverse',
+		'label' => __( 'Reverse order', 'mosne' ),
+	]
+);
+register_block_style(
+	'core/columns',
+	[
+		'name'  => 'grid-4',
+		'label' => __( 'Grid of 4', 'mosne' ),
+	]
+);
+register_block_style(
+	'core/columns',
+	[
+		'name'  => 'grid-4',
+		'label' => __( 'Grid of 4', 'mosne' ),
 	]
 );
